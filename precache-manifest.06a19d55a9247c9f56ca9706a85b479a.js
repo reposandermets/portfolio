64afceb38bd3905a8c80b209c4c5ffe0ac9bfac6
Sander Mets <sander@farmx.co>
2020-09-13 20:48:51 +0300

    Updates

---
+++ precache-manifest.06a19d55a9247c9f56ca9706a85b479a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be7c34fcb5db9704aec83768fb42799",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "808e3d249bbe80f16edb",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "41d1664ff38533b85ca3",
+    "url": "/portfolio/static/js/2.0385c622.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.0385c622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808e3d249bbe80f16edb",
+    "url": "/portfolio/static/js/main.076a41bf.chunk.js"
+  },
+  {
+    "revision": "a4575ecd265ae944a895",
+    "url": "/portfolio/static/js/runtime-main.2b6f91e6.js"
+  },
+  {
+    "revision": "45efc878a2ffcc18b4998b70729c2986",
+    "url": "/portfolio/static/media/fxlogo.45efc878.png"
+  },
+  {
+    "revision": "32f033df4fc637387b3694dc12a64b90",
+    "url": "/portfolio/static/media/sander.32f033df.png"
+  }
+]);
